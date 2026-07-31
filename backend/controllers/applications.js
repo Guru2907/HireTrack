7bd87a1c27867a24519d93dbae2fb60cdd25dfb2
Gurpreet <gurpreetsingh200529@gmail.com>
2026-07-31 15:17:19 +0530

    feat: completed creating crud routes

--- backend/controllers/applications.js
+++ backend/controllers/applications.js
@@ -1,20 +1,44 @@
 const Application = require('../models/Application');
 
 exports.getAll = async (req, res) => {
-  res.status(501).json({ message: 'getAll not implemented yet' });
+  try {
+    const applications = await Application.find({ user: req.user._id }).sort({ createdAt: -1 });
+    res.json(applications);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
 };
 
-// TODO — Plan Phase 1, Step 8: create application, attach req.user._id
 exports.create = async (req, res) => {
-  res.status(501).json({ message: 'create not implemented yet' });
+  try{
+    const applications = await Application.create({...req.body,user:req.user._id})
+    res.status(201).json({message:"Succesfully Created"})
+  }catch(err){
+    res.status(500).json({ message: err.message });
+  }
 };
-
 // TODO — Plan Phase 1, Step 8 + Phase 3: update doc, then emit Socket.io 'statusUpdate' event
 exports.update = async (req, res) => {
-  res.status(501).json({ message: 'update not implemented yet' });
+  try{
+    const applications = await Application.findOneAndUpdate({_id:req.params.id,user:req.user._id},req.body,{new :true})
+    if (!applications) {
+      return res.status(404).json({ message: "Not found" });
+    }
+    res.json(applications);
+  }catch(err){
+    res.status(500).json({ message: err.message });
+  }
 };
 
 // TODO — Plan Phase 1, Step 8: findOneAndDelete scoped to req.user._id
 exports.remove = async (req, res) => {
-  res.status(501).json({ message: 'remove not implemented yet' });
+  try{
+    const applications = await Application.findOneAndDelete({_id:req.params.id,user:req.user._id})
+    if (!applications) {
+      return res.status(404).json({ message: "Not found" });
+    }
+    res.status(200).json({ message: "Successfully Deleted" });
+  }catch(err){
+    res.status(500).json({ message: err.message });
+  }
 };
