40f77919f45fddccb2a0c1ddb661b16e983cb443
Gurpreet <gurpreetsingh200529@gmail.com>
2026-07-31 13:54:21 +0530

    feat: completed auth routes

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -1,13 +1,55 @@
 const User = require('../models/User');
-// const bcrypt = require('bcryptjs');
-// const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
-// TODO — Plan Phase 1, Step 6: hash password with bcrypt, create user, sign JWT (7d expiry)
 exports.signup = async (req, res) => {
-  res.status(501).json({ message: 'signup not implemented yet' });
+  try{
+    const {name,email,password} = req.body
+    const existingUser = await User.findOne({email})
+    if (existingUser){
+      return res.status(400).json({message:"Email Already Registered"})
+    }
+    const hashedPassword = await bcrypt.hash(password,12)
+    const newUser = new User({
+      name:name,
+      email:email,
+      password:hashedPassword,
+    })
+
+    await newUser.save()
+
+    const token = jwt.sign(
+      { id:newUser._id },
+      process.env.JWT_SECRET,
+      { expiresIn: "7d"}
+    )
+    res.status(201).json({token,name:newUser.name})
+  }catch(err){
+    res.status(500).json({ message: err.message });
+  }
 };
 
-// TODO — Plan Phase 1, Step 6: find user by email, compare password, sign JWT
 exports.login = async (req, res) => {
-  res.status(501).json({ message: 'login not implemented yet' });
+  try{
+    const {name,email,password} = req.body
+
+    const existingUser = await User.findOne({email})
+    if (!existingUser){
+      return res.status(400).json({message:"User Not Found"})
+    }
+
+    const isMatch = bcrypt.compare(password, existingUser.password)
+    if (!isMatch) {
+            return res.status(400).json({ message: "Invalid Credentials" })
+    }
+
+    const token = jwt.sign(
+      { id:existingUser._id },
+      process.env.JWT_SECRET,
+      { expiresIn: "7d"}
+    )
+    res.status(201).json({token,name:existingUser.name})
+  }catch(err){
+    res.status(500).json({ message: err.message });
+  }
 };
